ab49093e77c668e480160fcbcf3eed1d85c447de
Anton <mikhailov.ant@gmail.com>
2024-06-01 11:40:44 +0100

    app init
    [feat] packages -> add TS support, add and configure tailwind css, add & configure shadcn UI-library, add prettier
    [feat] _app.tsx -> general app component with UI library test button
    [fix] delete next template index.js && relevant css

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import '../styles/global.css';
+import { Button } from '@/components/ui/button';
+
+export default function MyApp() {
+  return (
+    <>
+      <p className="pb-2">Harmonica</p>
+      <Button>Say hi</Button>
+    </>
+  );
+}
