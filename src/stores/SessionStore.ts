7bbcf0f594e6a32cafcd326525a91c1ada551b6e
derjogi <derjogi@gmail.com>
2024-07-20 23:46:43 +0200

    Accumulate results on the overview page already

--- src/stores/SessionStore.ts
+++ src/stores/SessionStore.ts
@@ -1,9 +1,12 @@
 import { create } from 'zustand'
 import { SessionData } from '../app/home/page'
+import { AccumulatedSessionData } from 'lib/utils'
 
 interface SessionStore {
   sessions: Record<string, SessionData[]>
   setSessions: (id: string, data: SessionData[]) => void
+  accumulated: Record<string, AccumulatedSessionData>
+  setAccumulatedSessions: (id: string, data: AccumulatedSessionData) => void
 }
 
 export const useSessionStore = create<SessionStore>((set) => ({
@@ -11,4 +14,8 @@ export const useSessionStore = create<SessionStore>((set) => ({
   setSessions: (id, data) => set((state) => ({ 
     sessions: { ...state.sessions, [id]: data } 
   })),
+  accumulated: {},
+  setAccumulatedSessions: (id, data) => set((state) => ({
+    accumulated: { ...state.accumulated, [id]: data }
+  }))
 }))
