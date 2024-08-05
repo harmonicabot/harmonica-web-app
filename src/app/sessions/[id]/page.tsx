011f17e2a171ee72924490d033d56c8eeac25ce6
Anton <mikhailov.ant@gmail.com>
2024-08-05 22:58:51 +0100

    final_report
    [feat] session component -> update page after get final report, share final report with participants
    [feat] types -> add final report sent type

--- src/app/sessions/[id]/page.tsx
+++ src/app/sessions/[id]/page.tsx
@@ -1,66 +1,75 @@
 'use client';
 
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import { useSessionStore } from '@/stores/SessionStore';
-import Markdown from 'react-markdown'
+import Markdown from 'react-markdown';
 import { accumulateSessionData, sendApiCall } from 'utils/utils';
 
 export default function DashboardIndividual() {
   const { id } = useParams() as { id: string };
+  const router = useRouter();
 
-  const [accumulated, setAccumulated] = useSessionStore(
-    (state) => [
-      state.accumulated[id],
-      state.addAccumulatedSessions
-    ]
-  )
+  const [accumulated, setAccumulated] = useSessionStore((state) => [
+    state.accumulated[id],
+    state.addAccumulatedSessions,
+  ]);
 
   useEffect(() => {
-    console.log("useEffect triggered to fetch session data for ", id)
+    console.log('useEffect triggered to fetch session data for ', id);
     if (!accumulated) {
-      console.log("No data in store, fetching...")
+      console.log('No data in store, fetching...');
       // Fetch data from the database if not in store
-      fetchSessionData()
+      fetchSessionData();
     } else {
-      console.log("Session data found in store")
+      console.log('Session data found in store', accumulated);
     }
-  }, [id, accumulated])
+  }, [id, accumulated]);
 
   const fetchSessionData = async () => {
-    console.log(`Fetching session data for ${id}...`)
+    console.log(`Fetching session data for ${id}...`);
     const data = await sendApiCall({
-      action: "stats",
+      action: 'stats',
       data: {
         session_id: id,
       },
-    })
-    
-    setAccumulated(id, accumulateSessionData(data))
+    });
+
+    setAccumulated(id, accumulateSessionData(data));
+  };
+
+  const sendFinalReport = async () => {
+    const data = await sendApiCall({
+      action: 'send final report',
+      data: {
+        session_id: id,
+      },
+    });
+    await fetchSessionData();
   };
 
   const createSummary = async () => {
-    console.log(`Creating summary for ${id}...`)
+    console.log(`Creating summary for ${id}...`);
     const data = await sendApiCall({
-      action: "create summary",
+      action: 'create summary',
       data: {
         session_id: id,
-        finished: accumulated.session_data.finished
+        finished: accumulated.session_data.finished,
       },
     });
-    accumulated.session_data.summary = data;
+    await fetchSessionData();
   };
 
   const handleDelete = async () => {
-    console.log(`Deleting session ${id}...`)
+    console.log(`Deleting session ${id}...`);
     const data = await sendApiCall({
-      action: "delete",
+      action: 'delete',
       data: {
         session_id: id,
       },
     });
-    console.log(data)
-    window.location.href = "/"
+    console.log(data);
+    window.location.href = '/';
   };
 
   if (!accumulated) return <div>Loading...</div>;
@@ -71,40 +80,54 @@ export default function DashboardIndividual() {
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         <div className="bg-white p-4 rounded shadow">
           <h2 className="text-xl font-semibold">All Participants</h2>
-          <p className="text-3xl font-bold">{accumulated.session_data.num_sessions}</p>
+          <p className="text-3xl font-bold">
+            {accumulated.session_data.num_sessions}
+          </p>
         </div>
         <div className="bg-white p-4 rounded shadow">
           <h2 className="text-xl font-semibold">Active Participants</h2>
-          <p className="text-3xl font-bold">{accumulated.session_data.active}</p>
+          <p className="text-3xl font-bold">
+            {accumulated.session_data.active}
+          </p>
         </div>
         <div className="bg-white p-4 rounded shadow">
           <h2 className="text-xl font-semibold">Template</h2>
-          <p className="text-3xl font-bold">{accumulated.session_data.template || 'N/A'}</p>
+          <p className="text-3xl font-bold">
+            {accumulated.session_data.template || 'N/A'}
+          </p>
         </div>
         <div className="bg-white p-4 rounded shadow">
           <h2 className="text-xl font-semibold">Summary Available</h2>
-          <p className="text-3xl font-bold">{accumulated.session_data.summary ? 'Yes' : 'No'}</p>
+          <p className="text-3xl font-bold">
+            {accumulated.session_data.summary ? 'Yes' : 'No'}
+          </p>
         </div>
       </div>
       <div className="mt-6 bg-white p-4 rounded shadow">
         <h2 className="text-xl font-semibold mb-2">Summary</h2>
         {accumulated.session_data.summary ? (
-          <Markdown>{accumulated.session_data.summary}</Markdown>          
+          <Markdown>{accumulated.session_data.summary}</Markdown>
         ) : (
-          <button
-            className='bg-yellow-300'
-            onClick={createSummary}
-          >
+          <button className="bg-yellow-300" onClick={createSummary}>
             Create
           </button>
         )}
       </div>
-      <div className='m-6'>
-        <button className='bg-yellow-300'
-          onClick={handleDelete}
-        >
+      <div className="m-6">
+        <button className="bg-yellow-300" onClick={handleDelete}>
           Delete
         </button>
+        {accumulated.session_data.summary ? (
+          accumulated.session_data.finalReportSent ? (
+            <p>Final report has been sent</p>
+          ) : (
+            <button className="ms-4 bg-yellow-300" onClick={sendFinalReport}>
+              Send final report
+            </button>
+          )
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,21 +1,29 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import { AccumulatedSessionData, RawSessionData, RequestData } from 'utils/types';
-import type { NextApiRequest, NextApiResponse } from 'next' 
+import {
+  AccumulatedSessionData,
+  RawSessionData,
+  RequestData,
+} from 'utils/types';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function accumulateSessionData(data: RawSessionData): AccumulatedSessionData {
+export function accumulateSessionData(
+  data: RawSessionData,
+): AccumulatedSessionData {
   console.log('Raw session data:', data);
 
   const userSessions = Object.values(data.user_data);
   const total_sessions = userSessions.length;
   // This should always be a number, but the types between User&Session data aren't separated well enough... so let's handle both
-  const active = userSessions.filter((session) => typeof session.active === 'number' ? session.active > 0 : session.active).length;  
+  const active = userSessions.filter((session) =>
+    typeof session.active === 'number' ? session.active > 0 : session.active,
+  ).length;
   const finished = total_sessions - active;
-  
+
   const accumulated: AccumulatedSessionData = {
     session_data: {
       num_sessions: total_sessions,
@@ -25,6 +33,7 @@ export function accumulateSessionData(data: RawSessionData): AccumulatedSessionD
       template: data.session_data.template || '',
       topic: data.session_data.topic,
       context: data.session_data.context,
+      finalReportSent: data.session_data.final_report_sent,
     },
     user_data: data.user_data,
   };
