019fcc529ab7371c051b7df9f49d66a8331577ba
Anton <mikhailov.ant@gmail.com>
2024-09-24 18:24:33 +0100

    delete unused stuff

--- src/utils/amplify-server-utils.ts
+++
@@ -1,6 +0,0 @@
-import { createServerRunner } from "@aws-amplify/adapter-nextjs";
-import config from "../../amplify_outputs.json";
-
-export const { runWithAmplifyServerContext } = createServerRunner({
-  config,
-});
