b6c04f61f9f7b86b3dca694cd46dd7de4b2c8f49
Anton <mikhailov.ant@gmail.com>
2024-09-27 15:53:51 +0100

    v1
    [fix] dashboard component -> all tabs are hidden now
    [feat] session component -> make session name clickable, delete on click event from the button
    [feat] create page component -> update main card with on the last step, update copy
    [feat] review component -> add modal with full prompt
    [feat] share component -> update UI, add copy to clipboard function
    [feat] login page component -> delete github, gmail login buttons, email login logic added
    [feat] auth lib -> add emeil provider based on the email list
    [feat] middleware -> extract chat page and api chat calls from auth check

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,30 @@
-import NextAuth from 'next-auth';
+import NextAuth, { User } from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 import Google from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
 import type { Provider } from 'next-auth/providers';
 
-const providers: Provider[] = [GitHub, Google];
+const approvedEmails = ['hello@harmonica.chat', 'user@example.com'];
+
+const customEmailProvider = CredentialsProvider({
+  id: 'email',
+  name: 'Email',
+  credentials: {
+    email: { label: 'Email', type: 'email' },
+    password: { label: 'Password', type: 'password' },
+  },
+  authorize: async (credentials: { email: string; password: string }) => {
+    const { email, password } = credentials;
+    if (approvedEmails.includes(email) && password === 'password') {
+      return { id: email, email } as User;
+    }
+    return null;
+  },
+});
+
+const providers: Provider[] = [GitHub, Google, customEmailProvider];
+
 export const providerMap = providers
   .filter((provider) => typeof provider === 'function')
   .map((provider) => {
@@ -19,7 +40,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   callbacks: {
     authorized: async ({ auth }) => {
       // Logged in users are authenticated, otherwise redirect to login page
-      return true;
+      return !!auth;
     },
   },
 });
