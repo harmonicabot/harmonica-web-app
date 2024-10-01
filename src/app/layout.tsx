b2ea7bd6dac066a25fd69900658125b82d7c7fda
Anton <mikhailov.ant@gmail.com>
2024-10-01 20:18:11 +0100

    fixes-01-10
    [fix] app layout -> update Help button link
    [fix] icons -> fix svg parameter names

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,11 @@ export default function RootLayout({
               <Logo />
             </Link>
             <div className="flex items-center space-x-4">
-              <Link href="https://harmonica.chat/support">
+              <Link
+                href="https://oldspeak.notion.site/Help-Center-fcf198f4683b4e3099beddf48971bd40"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <button className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50">
                   Help
                 </button>
