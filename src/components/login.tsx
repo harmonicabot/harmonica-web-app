3d31ff36149a09adb7e376f46f530b42b4d4fe0b
Jonas <derjogi@web.de>
2024-10-18 17:08:31 +1300

    HRM-293 Logout Button
    Bonus: Login with 'enter'

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -83,6 +83,11 @@ export default function Login() {
                 type="password"
                 value={password}
                 onChange={handlePasswordChange}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    handleLoginClick();
+                  }
+                }}
                 required
               />
               {error && <div className="text-red-500 text-center">{error}</div>}
--- src/components/user.tsx
+++ src/components/user.tsx
@@ -10,51 +10,68 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import Link from 'next/link';
+import { UsersIcon } from './icons';
 
 export async function User() {
   let session = await auth();
   let user = session?.user;
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        {/* <Button
-          variant="outline"
-          size="icon"
-          className="overflow-hidden rounded-full"
-        >
-          <Image
-            src={user?.image ?? '/placeholder-user.jpg'}
-            width={36}
-            height={36}
-            alt="Avatar"
-            className="overflow-hidden rounded-full"
-          />
-        </Button> */}
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        {/* <DropdownMenuSeparator /> */}
-        {/* <DropdownMenuItem>Settings</DropdownMenuItem>
-        <DropdownMenuItem>Support</DropdownMenuItem> */}
-        <DropdownMenuSeparator />
-        {user ? (
-          <DropdownMenuItem>
-            <form
-              action={async () => {
-                'use server';
-                await signOut();
-              }}
-            >
-              <button type="submit">Sign Out</button>
-            </form>
-          </DropdownMenuItem>
-        ) : (
-          <DropdownMenuItem>
-            <Link href="/login">Sign In</Link>
-          </DropdownMenuItem>
-        )}
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      {user && (
+        <form action={async () => {
+          'use server';
+          await signOut();
+        }}>
+          <Button
+            variant="link"
+            type="submit"
+          >
+            {'Sign Out'}            
+          </Button>
+        </form>
+      )}
+    </>
+
+    // <DropdownMenu>
+    //   <DropdownMenuTrigger asChild>
+    //     {/* <Button
+    //       variant="outline"
+    //       size="icon"
+    //       className="overflow-hidden rounded-full"
+    //     >
+    //       <Image
+    //         src={user?.image ?? '/placeholder-user.jpg'}
+    //         width={36}
+    //         height={36}
+    //         alt="Avatar"
+    //         className="overflow-hidden rounded-full"
+    //       />
+    //     </Button> */}
+    //   </DropdownMenuTrigger>
+    //   <DropdownMenuContent align="end">
+    //     <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    //     {/* <DropdownMenuSeparator /> */}
+    //     {/* <DropdownMenuItem>Settings</DropdownMenuItem>
+    //     <DropdownMenuItem>Support</DropdownMenuItem> */}
+    //     <DropdownMenuSeparator />
+    //     {user ? (
+    //       <DropdownMenuItem>
+    //         <form
+    //           action={async () => {
+    //             'use server';
+    //             await signOut();
+    //           }}
+    //         >
+    //           <button type="submit">Sign Out</button>
+    //         </form>
+    //       </DropdownMenuItem>
+    //     ) : (
+    //       <DropdownMenuItem>
+    //         <Link href="/login">Sign In</Link>
+    //       </DropdownMenuItem>
+    //     )}
+    //   </DropdownMenuContent>
+    // </DropdownMenu>
   );
 }
