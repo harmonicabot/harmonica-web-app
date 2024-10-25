deb6a8c352a7c7c6c14137c2bca6f6da5aba0d0a
Jonas <derjogi@gmail.com>
2024-10-25 21:22:31 +1300

    HOTFIX - Remove Amplify

--- amplify/functions/builder/resource.ts
+++
@@ -1,8 +0,0 @@
-import { defineFunction } from '@aws-amplify/backend';
-
-export const templateBuilder = defineFunction({
-  // optionally specify a name for the Function (defaults to directory name)
-  name: 'templateBuilder',
-  // optionally specify a path to your handler (defaults to "./handler.ts")
-  entry: './builder.ts'
-});
--- amplify/functions/testing/handler.ts
+++
@@ -1,8 +0,0 @@
-import type { Handler } from 'aws-lambda';
-import type { Schema } from '../../data/resource';
-
-export const handler: Schema["testing"]["functionHandler"] = async (event, context) => {
-  // your function code goes here
-  const { name, something } = event.arguments
-  return `Hello ${name}! I'm just testing ${something}.`;
-};
