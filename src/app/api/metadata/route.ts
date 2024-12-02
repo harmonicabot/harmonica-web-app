432ab3083169a11b018b236c5488a478196a49af
Jonas <derjogi@gmail.com>
2024-12-02 16:45:40 +1300

    HRM-368 Metadata
    * Trying yet another way

--- src/app/api/metadata/route.ts
+++ src/app/api/metadata/route.ts
@@ -1,31 +1,9 @@
-import { decryptId } from '@/lib/encryptionUtils'
-import { routeMetadata } from './metadata'
+import { getGeneratedMetadata } from './metadata'
 import { NextResponse } from 'next/server'
-import * as db from '@/lib/db'
 
 export async function GET(request: Request) {
-  const url = new URL(request.url)
-  const path = url.pathname.replace('/api/metadata', '')
-  let title = 'Harmonica Dashboard';
-  // Handle dynamic session routes
-  if (path.startsWith('/sessions/')) {
-    const sessionId = path.split('/')[2]
-    const decryptedId = decryptId(sessionId)
-    const hostData = await db.getHostSessionById(decryptedId)
-    
-    title = hostData.topic;
-  }
-  
-  const metadata = routeMetadata[path] || routeMetadata['/']
-  
-
-  return NextResponse.json({
-    ...metadata,
-    title,
-    openGraph: {
-      ...metadata,
-      type: 'website',
-      url: `${url.host}${path}`
-    }
-  })
+  console.log('Metadata request');
+  const metadata = await getGeneratedMetadata(new URL(request.url).pathname);
+  console.log('Generated Metadata: ', metadata)
+  return NextResponse.json(metadata);
 }
--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,4 +1,7 @@
+import { getGeneratedMetadata } from "app/api/metadata/metadata";
 import CreationFlow from "./creationFlow";
+
+export const metadata = getGeneratedMetadata('/create');
 
 export default function Create() {
   return <CreationFlow/>
