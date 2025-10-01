dc99cba21d33b37638231e5ca42b3b8b8381d362
Jonas <derjogi@gmail.com>
2025-10-01 16:56:45 +1300

    Fix: Make dynamic preview branches work

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,3 +1,9 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { initAuth0 } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+const baseURL = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : process.env.AUTH0_BASE_URL;
+
+const auth0 = initAuth0({ baseURL });
+
+export const { GET, POST } = auth0.handleAuth();
