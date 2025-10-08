eacb1b5d100ad5c3c6cdcb70d97ac33bd244bac8
Jonas <derjogi@gmail.com>
2025-10-08 17:28:00 +1300

    Revert changes to auth

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,9 +1,3 @@
-import { initAuth0 } from '@auth0/nextjs-auth0';
+import { handleAuth } from '@auth0/nextjs-auth0';
 
-const baseURL = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : process.env.AUTH0_BASE_URL;
-
-const auth0 = initAuth0({ baseURL });
-
-export const { GET, POST } = auth0.handleAuth();
+export const GET = handleAuth();
